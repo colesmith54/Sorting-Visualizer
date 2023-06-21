eda8578634bb0a9590105b9f4490f9ffa6f66251
Cole Smith <114085318+colesmith54@users.noreply.github.com>
2023-06-21 00:32:49 +0000

    Added optimized code section

--- algo/javascript/globals.js
+++ algo/javascript/globals.js
@@ -54,6 +54,56 @@ const algoSortCode = {
             }
         }
     }
+
+    return arr;
+}`,
+    'selection': `function selectionSort(arr) {
+
+    const len = arr.length;
+
+    for(let i = 0; i < len; i++) {
+
+        // Assume the minimum is the first element
+        let min = i;
+
+        for (let j = i + 1; j < len; j++) {
+
+            // Check for new minimum value
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+
+        // Swap min element into position
+        if (i !== min) {
+            let temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    return arr;
+}`,
+
+'insertion': `function insertionSort(arr) {
+
+    const len = arr.length;
+
+    // Loop through every element
+    for(let i = 1; i < len; i++) {
+
+        let j = i;
+
+        // Swap adjacent elements until the correct 
+        // position is found for the current item
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            let temp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = temp;
+            j--;
+        }
+    }
+
     return arr;
 }`
 }
@@ -80,6 +130,31 @@ const optimizedAlgoSortCode = {
             }
         }
     } while (swapped)
+    return arr;
+} 
+//see "Cocktail Sort" for more optimization`,
+    'selection': `see "Minmax Sort"`,
+
+    'insertion': `function insertionSort(arr) {
+
+    const len = arr.length;
+
+    for (let i = 1; i < len; i++) {
+
+        // Store the current value so it can be placed correctly
+        let current = arr[i];
+
+        // Instead of swapping every time, find the
+        // correct location and then perform the swap
+        let j = i - 1;
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        // Place the current item in the correct spot
+        arr[j + 1] = current;
+    }
     return arr;
 }`
 }
