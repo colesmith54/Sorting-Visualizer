dd6a0af518e298c2609b1d7abcc252716b36b7d1
Cole Smith <114085318+colesmith54@users.noreply.github.com>
2023-06-22 03:19:53 +0000

    Added dynamic color coding to table cells

--- algo/javascript/descriptions.js
+++ algo/javascript/descriptions.js
@@ -1,44 +1,44 @@
 const descriptions = {
     'bubble': {
         description: 'Bubble Sort is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted.',
-        bestTime: 'O(n)',
-        avgTime: 'O(n^2)',
-        worstTime: 'O(n^2)',
-        space: 'O(1)'
+        bestTime: 'n',
+        avgTime: 'n^2',
+        worstTime: 'n^2',
+        space: '1'
     },
     'selection': {
         description: 'Selection Sort is a simple sorting algorithm that divides the input list into a sorted and an unsorted region, and repeatedly picks the smallest element from the unsorted region and moves it to the beginning of the sorted region.',
-        bestTime: 'O(n^2)',
-        avgTime: 'O(n^2)',
-        worstTime: 'O(n^2)',
-        space: 'O(1)'
+        bestTime: 'n^2',
+        avgTime: 'n^2',
+        worstTime: 'n^2',
+        space: '1'
     },
     'insertion': {
         description: 'Insertion Sort is a simple sorting algorithm that builds the final sorted list one item at a time. For each item, insertion sort will iterate back through the previous items until the correct position is found.',
-        bestTime: 'O(n)',
-        avgTime: 'O(n^2)',
-        worstTime: 'O(n^2)',
-        space: 'O(1)'
+        bestTime: 'n',
+        avgTime: 'n^2',
+        worstTime: 'n^2',
+        space: '1'
     },
     'minMax': {
         description: 'MinMax Sort is a comparison sorting algorithm where the maximum and minimum are found simultaneously, and then put in their proper places. The process is repeated for the remaining sublist.',
-        bestTime: 'O(n^2)',
-        avgTime: 'O(n^2)',
-        worstTime: 'O(n^2)',
-        space: 'O(1)'
+        bestTime: 'n^2',
+        avgTime: 'n^2',
+        worstTime: 'n^2',
+        space: '1'
     },
     'cocktail': {
         description: 'Cocktail Sort, also known as bidirectional bubble sort, is a variation of bubble sort. It differs in that it sorts in both directions each pass through the list, which usually results in a quicker sort.',
-        bestTime: 'O(n)',
-        avgTime: 'O(n^2)',
-        worstTime: 'O(n^2)',
-        space: 'O(1)'
+        bestTime: 'n',
+        avgTime: 'n^2',
+        worstTime: 'n^2',
+        space: '1'
     },
     'heap': {
         description: 'Heap Sort is a comparison-based sorting algorithm that uses a binary heap data structure. The algorithm orders the elements by first building a max heap and then repeatedly extracts the maximum from the heap and swaps it with the last unsorted element, reducing the size of the heap by one each time.',
-        bestTime: 'O(n log(n))',
-        avgTime: 'O(n log(n))',
-        worstTime: 'O(n log(n))',
-        space: 'O(1)'
+        bestTime: 'n log(n)',
+        avgTime: 'n log(n)',
+        worstTime: 'n log(n)',
+        space: '1'
     }
 };
--- algo/javascript/update.js
+++ algo/javascript/update.js
@@ -1,17 +1,60 @@
+function updateComplexityCell(cell, complexity) {
+    cell.innerText = `O(${complexity})`;
+    switch (complexity) {
+        case '1':
+            cell.style.border = '2px solid #008000';
+            break;
+        case 'log n':
+            cell.style.border = '2px solid #ADFF2F';
+            break;
+        case 'n':
+            cell.style.border = '2px solid #FFD700';
+            break;
+        case 'n log(n)':
+            cell.style.border = '2px solid #FF8C00';
+            break;
+        case 'n^2':
+        default:
+            cell.style.border = '2px solid red';
+            break;
+    }
+}
+
+function adjustBorder(firstElement, secondElement) {
+    if (firstElement.style.borderColor === secondElement.style.borderColor) {
+        firstElement.style.borderRightWidth = "1px";
+        secondElement.style.borderLeftWidth = "1px";
+    } else {
+        firstElement.style.borderRightWidth = "2px";
+        secondElement.style.borderLeftWidth = "2px";
+    }
+}
+
 function updateAlgorithmDetails(algorithmKey) {
     const algorithmInfo = descriptions[algorithmKey];
     if (algorithmInfo) {
         document.getElementById('algorithm-title').innerText = `${algorithmKey.charAt(0).toUpperCase()}${algorithmKey.slice(1)} Sort`;
         document.getElementById('algorithm-description').innerText = algorithmInfo.description;
-        document.getElementById('best-case').innerText = algorithmInfo.bestTime;
-        document.getElementById('average-case').innerText = algorithmInfo.avgTime;
-        document.getElementById('worst-case').innerText = algorithmInfo.worstTime;
-        document.getElementById('space-complexity').innerText = algorithmInfo.space;
+
+        const bestCaseElement = document.getElementById('best-case');
+        const averageCaseElement = document.getElementById('average-case');
+        const worstCaseElement = document.getElementById('worst-case');
+        const spaceComplexityElement = document.getElementById('space-complexity');
+
+        updateComplexityCell(bestCaseElement, algorithmInfo.bestTime);
+        updateComplexityCell(averageCaseElement, algorithmInfo.avgTime);
+        updateComplexityCell(worstCaseElement, algorithmInfo.worstTime);
+        updateComplexityCell(spaceComplexityElement, algorithmInfo.space);
+
+        adjustBorder(bestCaseElement, averageCaseElement);
+        adjustBorder(averageCaseElement, worstCaseElement);
+        adjustBorder(worstCaseElement, spaceComplexityElement);
 
         updateCodeDisplay('#code code', algoSortCode[algorithmKey]);
         updateCodeDisplay('#optimized-code code', optimizedAlgoSortCode[algorithmKey]);
     }
 }
+
 
 function updateCodeDisplay(selector, code) {
     let codeElement = document.querySelector(selector);
